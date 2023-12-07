5752716956360e44d584965b8dfcec0647709142
Abhishek Vashisth <100755099+ABHISHEK-VASHISTH@users.noreply.github.com>
2023-12-07 14:02:46 +0530

    Create SERVER.py
    It is the server code

---
+++ SERVER.py
@@ -0,0 +1,35 @@
+import time, socket, sys
+
+print("\nWelcome to Chat Room\n")
+print("Initialising....\n")
+time.sleep(1)
+
+s = socket.socket()
+host = socket.gethostname()
+ip = socket.gethostbyname(host)
+port = 1234
+s.bind((host, port))
+print(host, "(", ip, ")\n")
+name = input(str("Enter your name: "))
+
+s.listen(1)
+print("\nWaiting for incoming connections...\n")
+conn, addr = s.accept()
+print("Received connection from ", addr[0], "(", addr[1], ")\n")
+
+s_name = conn.recv(1024)
+s_name = s_name.decode()
+print(s_name, "has connected to the chat room\nEnter [e] to exit chat room\n")
+conn.send(name.encode())
+
+while True:
+    message = input(str("Me : "))
+    if message == "[e]":
+        message = "Left chat room!"
+        conn.send(message.encode())
+        print("\n")
+        break
+    conn.send(message.encode())
+    message = conn.recv(1024)
+    message = message.decode()
+    print(s_name, ":", message)
